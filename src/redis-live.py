3baa3c6827a9c3ec4491070b2e622d71391a9dda
Sushanto Pandit <sushantopandit@users.noreply.github.com>
2022-08-14 20:41:44 -0700

    Update

--- src/redis-live.py
+++ src/redis-live.py
@@ -15,6 +15,7 @@ from api.controller.TopCommandsController import TopCommandsController
 from api.controller.TopKeysController import TopKeysController
 
 if __name__ == "__main__":
+    define("server", default=localhost, help="run on the given host", type=string)
     define("port", default=63790, help="run on the given port", type=int)
     define("debug", default=0, help="debug mode", type=int)
     tornado.options.parse_command_line()
